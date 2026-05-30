7dc8877390272bcb1125b3e5173b572813a75b90
DimasHadiNugraha <hp.vivo7410@gmail.com>
2026-05-30 19:37:05 +0700

    mengembalikan structures.py

---
+++ utils/structures.py
@@ -0,0 +1,5 @@
+from utils.journal       import JournalNode, DreamJournal       # Singly Linked List
+from utils.nightmare_loop import DreamNode, NightmareLoop        # Circular Linked List
+from utils.vault         import VaultNode, MemoryVault           # Double Linked List
+from utils.memory_stack  import MemoryStack                      # Stack LIFO
+from utils.hashing       import HashTable                        # Hash Table untuk rahasia
